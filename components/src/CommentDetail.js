aaaaa54e085bff54195354ede981ee0afc19391e
kivvvvv <kivistheslayer@gmail.com>
2019-07-14 10:04:46 +0700

    Lesson 36: Passing Props - Solutions

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from "react";
-import faker from "faker";
 
-export default function CommentDetail() {
+export default function CommentDetail(props) {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img src={props.avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
