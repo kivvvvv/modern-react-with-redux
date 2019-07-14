486ec5a52c7cbc98bbea4833370e0adaa673278d
kivvvvv <kivistheslayer@gmail.com>
2019-07-14 10:45:22 +0700

    Lesson 40: Component Reuse

--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,15 @@ import ApprovalCard from "./ApprovalCard";
 function App() {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>Are you sure?</ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
