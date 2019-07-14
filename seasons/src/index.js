210d2e28fdbe4ffda3ee2199f00baee0631024ad
kivvvvv <kivistheslayer@gmail.com>
2019-07-15 06:10:02 +0700

    Lesson 56: Handling Errors Gracefully

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,18 +4,27 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: null };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         console.log(position);
         this.setState({ lat: position.coords.latitude });
       },
-      error => console.log(error)
+      error => {
+        console.log(error);
+        this.setState({ errorMessage: error.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
