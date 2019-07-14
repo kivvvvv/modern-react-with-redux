79e8e7ac4114657eb3144d7a0e769cd4e1db6b72
kivvvvv <kivistheslayer@gmail.com>
2019-07-14 21:18:02 +0700

    Lesson 53: Initializing State Through Constructors

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { lat: null };
+  }
   render() {
     let latitute;
 
