fc4f771aa0f1957505c1174e1a3bedd03e882092
kivvvvv <kivistheslayer@gmail.com>
2019-07-27 13:11:41 +0700

    Lesson 63: Determining Season

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function getSeason(lat, month) {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+}
+
+export default function SeasonDisplay(props) {
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  return <div>Season Display</div>;
+}
