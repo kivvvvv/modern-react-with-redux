c02a04186f5b7cf1340f007fd65b1e4095ed3451
kivvvvv <kivistheslayer@gmail.com>
2019-07-27 13:07:58 +0700

    Lesson 61: Alternate State Intialization

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { lat: null, errorMessage: null };
-  }
+  state = { lat: null, errorMessage: null };
+
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
