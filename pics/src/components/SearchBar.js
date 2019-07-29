7943e5ea9ad7ae5bc3301f38bf6336b7f3e19d92
kivvvvv <kivistheslayer@gmail.com>
2019-07-29 08:02:28 +0700

    Lesson 81: Uncontrolled vs Controlled Elements

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-  handleInputChange(e) {
-    console.log(e.target.value);
-  }
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.handleInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
