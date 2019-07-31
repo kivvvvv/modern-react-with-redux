7157375de1ce6b04d20151da84982531f299385c
kivvvvv <kivistheslayer@gmail.com>
2019-07-31 09:21:15 +0700

    Lesson 90: Viewing Request Results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export default class App extends Component {
   handleSearchBarSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID c23cb4e244e44bacfbbdc03d6f2a714bb8a25b3d2fbf591b83affcc31bc0dbed"
+      }
+    });
   }
 
   render() {
