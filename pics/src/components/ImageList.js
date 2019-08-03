783edb0e6f2727252ff69f69b19c876ec90ade1c
Sirapop Iam-o-pas <sirapop@mysocialmotion.org>
2019-08-04 05:43:17 +0700

    Lesson 100: Grid CSS

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./ImageList.css";
 
 export default function ImageList(props) {
   const images = props.images.map(({ description, id, urls }) => (
     <img src={urls.regular} alt={description} key={id} />
   ));
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 }
